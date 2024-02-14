63b7772bc71ca1b7a7ac37db9667347403aee85f
Shreya Jaiswal <shreyajaiswal1604@gmail.com>
2024-02-14 17:23:36 -0500

    check-yml

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 terraform {
   required_providers {
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
       version = "5.16.0"
     }
   }
@@ -10,17 +10,17 @@ terraform {
 
 #Provider Configuration
 provider "google" {
-  credentials = file("${path.module}/key.json")
-  project     = var.project_id #"csy-6225-sj" # TODO: this can become as variable name, use the variables.tf file name and enter the key and value in this file
-  region      = var.region #"us-east1" # TODO: this can become as variable name, use the variables.tf file name and enter the key and value in this file
+  #credentials = file("${path.module}/key.json")
+  project = var.project_id #"csy-6225-sj" # TODO: this can become as variable name, use the variables.tf file name and enter the key and value in this file
+  region  = var.region     #"us-east1" # TODO: this can become as variable name, use the variables.tf file name and enter the key and value in this file
 }
 ############################################################################
 
 # VPCs, Subnets, and Routes
 locals {
-  vpc_names   = [for i in range(var.num_vpcs) : "${var.name}-vpc-${i + 1}"]
-  subnet_count = 2  # Number of subnets per VPC
-  type        = ["webapp", "db"]  # Example types for subnets
+  vpc_names    = [for i in range(var.num_vpcs) : "${var.name}-vpc-${i + 1}"]
+  subnet_count = 2                # Number of subnets per VPC
+  type         = ["webapp", "db"] # Example types for subnets
 }
 
 resource "google_compute_network" "vpc_network" {
@@ -29,7 +29,7 @@ resource "google_compute_network" "vpc_network" {
     auto_create_subnetworks         = false
     routing_mode                    = "REGIONAL"
     delete_default_routes_on_create = true
-  }}
+  } }
 
   name                            = each.value.name
   auto_create_subnetworks         = each.value.auto_create_subnetworks
@@ -40,32 +40,32 @@ resource "google_compute_network" "vpc_network" {
 resource "google_compute_subnetwork" "subnets" {
   for_each = google_compute_network.vpc_network
 
-  name                     = "${each.key}-webapp-subnet"
-  ip_cidr_range            = var.ip_cidr_range[0]
-  region                   = var.region
-  network                  = each.value.self_link
+  name          = "${each.key}-webapp-subnet"
+  ip_cidr_range = var.ip_cidr_range[0]
+  region        = var.region
+  network       = each.value.self_link
   #private_ip_google_access = true
 }
 
 resource "google_compute_subnetwork" "subnets_db" {
   for_each = google_compute_network.vpc_network
 
-  name                     = "${each.key}-db-subnet"
-  ip_cidr_range            = var.ip_cidr_range[1]
-  region                   = var.region
-  network                  = each.value.self_link
+  name          = "${each.key}-db-subnet"
+  ip_cidr_range = var.ip_cidr_range[1]
+  region        = var.region
+  network       = each.value.self_link
   #private_ip_google_access = false
 }
 
 resource "google_compute_route" "webapp_route" {
   for_each = google_compute_network.vpc_network
 
-  name            = "${each.key}-webapp-route"
-  network         = each.value.self_link
-  dest_range      = "0.0.0.0/0"
+  name             = "${each.key}-webapp-route"
+  network          = each.value.self_link
+  dest_range       = "0.0.0.0/0"
   next_hop_gateway = "default-internet-gateway"
-  priority        = 1000
-  tags = ["webapp"]
+  priority         = 1000
+  tags             = ["webapp"]
 }
 
 
--- variables.tf
+++ variables.tf
@@ -1,29 +1,29 @@
 variable "project_id" {
-  type           = string
-  description  = "Project ID"
-  default        = "csy-6225-sj"
+  type        = string
+  description = "Project ID"
+  default     = "csy-6225-sj"
 }
 
 variable "region" {
-  type           = string
-  description  = "Region for this infrastructure"
-  default        = "us-central1"
+  type        = string
+  description = "Region for this infrastructure"
+  default     = "us-central1"
 }
 
 variable "name" {
-  type           = string
-  description  = "Name for this infrastructure"
-  default       = "cloud-2024"
+  type        = string
+  description = "Name for this infrastructure"
+  default     = "cloud-2024"
 }
 
-variable"ip_cidr_range" {
-type=list(string)
-description="List of The range of internal addresses that are owned by this subnetwork."
-default=["10.10.10.0/24", "10.10.20.0/24"]
+variable "ip_cidr_range" {
+  type        = list(string)
+  description = "List of The range of internal addresses that are owned by this subnetwork."
+  default     = ["10.10.10.0/24", "10.10.20.0/24"]
 }
 
 variable "num_vpcs" {
   type        = number
   description = "Number of VPCs to create"
-  default = 2
+  default     = 2
 }
