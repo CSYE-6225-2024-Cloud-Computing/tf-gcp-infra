9a19d9797a4902ec5fc05768dd48dd0a6723795b
Shreya Jaiswal <shreyajaiswal1604@gmail.com>
2024-04-10 20:11:52 -0400

    working-infra-v1

--- provider.tf
+++ provider.tf
@@ -1,15 +1,25 @@
 terraform {
-
+  # required_providers {
+  #   google = {
+  #     source  = "hashicorp/google"
+  #     version = "5.16.0"
+  #   }
+  # }
   required_providers {
     google = {
       source  = "hashicorp/google"
       version = "~>5"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "~>4"
     }
   }
 }
 
 #Provider Configuration
 provider "google" {
-  project = var.project_id
-  region  = var.region
+  #credentials = file("${path.module}/key.json")
+  project = var.project_id #"csy-6225-sj" # TODO: this can become as variable name, use the variables.tf file name and enter the key and value in this file
+  region  = var.region     #"us-east1" # TODO: this can become as variable name, use the variables.tf file name and enter the key and value in this file
 }
